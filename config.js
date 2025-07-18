f771b3798c76caafa029239a6163ee64b03da379
FankFerd <155588609+FankFerd1988@users.noreply.github.com>
2025-07-18 18:12:24 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 // config.js - ไฟล์การตั้งค่า
-const LIFF_ID = '2007779014-YEMk4Ljl';
+const LIFF_ID = '2007779016-LdJY54ZJ';
 const API_URL = 'https://script.google.com/macros/s/AKfycby39G6OpnVEfAvaFqDD-vlzWoNbylLYblwjaHpxbIloVfwmRB68Sl39g87gswijuSag/exec';
 const GOLD_PRICE_API = 'https://script.google.com/macros/s/AKfycbwgvstkxFOR9p6zOV2d8iEGagbpQ6h8C3BhPnDCoB56jvmbAwSG0A9a36r6oRxNkBXQ/exec';
 const GRAMS_PER_BAHT = 15.244;
